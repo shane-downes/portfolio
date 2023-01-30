500ccaf3cc1a96a0620999f1e1a7a6fa8cf65103
shanedownes <shanedownes01@gmail.com>
2023-01-30 04:02:55 +0000

    Make z-index of SlideMenu greater than Navbar

--- src/componenets/SlideMenu.tsx
+++ src/componenets/SlideMenu.tsx
@@ -11,7 +11,7 @@ type SlideMenuProps = {
 export const SlideMenu = ({ open, setOpen }: SlideMenuProps) => {
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Dialog as="div" className="relative z-50" onClose={setOpen}>
         <Transition.Child
           as={Fragment}
           enter="ease-in-out duration-500"
